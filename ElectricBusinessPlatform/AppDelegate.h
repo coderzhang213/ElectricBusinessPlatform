e705b890b6dfd8988e11b47f73e89bca4555c7df
zhangyue <zhangyue@sancell.cn>
2020-05-15 17:00:26 +0800

    ...电商架构初始化

---
+++ ElectricBusinessPlatform/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ElectricBusinessPlatform
+//
+//  Created by Owen on 2020/5/15.
+//  Copyright © 2020 Owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ElectricBusinessPlatform/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ElectricBusinessPlatform
+//
+//  Created by Owen on 2020/5/15.
+//  Copyright © 2020 Owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
